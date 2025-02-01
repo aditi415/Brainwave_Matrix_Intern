adfcb0942513732629b87ed0ce6131b78c9eeee2
Aditi Bhosure <bhosure.as2003@gmail.com>
2025-02-01 20:48:41 +0530

    article_model.dart

---
+++ News-Reader-App/lib/article_model.dart
@@ -0,0 +1,22 @@
+class Article {
+  final String title;
+  final String description;
+  final String url;
+  final String imageUrl;
+
+  Article({
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.imageUrl,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      title: json['title'] ?? 'No Title',
+      description: json['description'] ?? 'No Description',
+      url: json['url'] ?? '',
+      imageUrl: json['urlToImage'] ?? '',
+    );
+  }
+}
